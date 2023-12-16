4269f55f3c0cec6b8fee35fde48e00e2505bfa7b
Ulia <cs-gorodok@yandex.ru>
2023-12-16 18:12:26 +0400

    src

--- src/test/java/ru/netology/Card.java
+++ src/test/java/ru/netology/Card.java
@@ -1,4 +1,26 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class Card {
+
+    @Test
+    void lastnamefirstnamephonecheckbox() {
+        open("http://localhost:8080");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванова Лидия");
+        form.$("[data-test-id=phone] input").setValue("+71111111111");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+
+
+
 }
